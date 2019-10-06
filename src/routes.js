a5ca6ce85a7ca8dfef0342c4df5c9cdf27c122a1
Jonathan Matheus <jonathanmatheus@MacBook-de-Jonathan.local>
2019-10-06 17:58:54 -0300

    Criando migrations de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-import { Router } from "express";
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get(`/`, (req, res) => {
-  return res.json({ message: `Hello Rocktseat` });
+routes.get(`/`, async (req, res) => {
+  const user = await User.create({
+    name: 'Jonathan Matheus',
+    email: 'jonathan@gmail.com',
+    password_hash: '12345667',
+  });
+  return res.json(user);
 });
 
 export default routes;
