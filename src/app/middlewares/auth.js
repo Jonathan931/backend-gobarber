5fe4b9e896f7c239b4bfc957d3512a6c936f981f
Jonathan Matheus <jonathanmatheus@MacBook-de-Jonathan.local>
2019-11-09 23:50:54 -0300

    Adicionando criação de login e usuário

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provider' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    const { id } = decoded;
+    req.userId = id;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '40c24952b445977ac86fb80a165c6c73',
+  expiresIn: '7d',
+};
