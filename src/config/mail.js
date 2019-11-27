6a6c54abd62c56319353506fb9e7f7a6435c837a
Jonathan Matheus <jonathanmatheus@MacBook-de-Jonathan.local>
2019-11-27 05:46:44 -0300

    Criando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0f9dc985d94c8d',
+    pass: '5ebafc1a9dcf8d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
